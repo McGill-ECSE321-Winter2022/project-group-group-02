a047948785abb85861bf6d3d19ce3126110392e3
KRouhana <37153041+KRouhana@users.noreply.github.com>
2022-02-16 13:29:35 -0500

    Interfaces for OrderItem and Order Model Classes
    Using CRUD

---
+++ GroceryStore-Backend/src/main/java/ca/mcgill/ecse321/GroceryStoreBackend/dao/OrderItemRepository.java
@@ -0,0 +1,21 @@
+package ca.mcgill.ecse321.GroceryStoreBackend.dao;
+
+import java.util.List;
+import org.springframework.data.repository.CrudRepository;
+import ca.mcgill.ecse321.GroceryStoreBackend.model.Order;
+import ca.mcgill.ecse321.GroceryStoreBackend.model.OrderItem;
+import ca.mcgill.ecse321.GroceryStoreBackend.model.ShoppableItem;
+
+
+public interface OrderItemRepository extends CrudRepository<OrderItem, Integer>  {
+
+  List<OrderItem> findByOrderAndShoppableItem(Order orderId, ShoppableItem itemName);
+  
+  boolean existsByOrderAndShoppableItem(Order orderId, ShoppableItem itemName);
+
+  OrderItem findOrderItemByOrderAndShoppableItem(Order orderId, ShoppableItem itemName);
+  
+  OrderItem findOrderItemById(Integer orderItemId);
+  
+  
+}
