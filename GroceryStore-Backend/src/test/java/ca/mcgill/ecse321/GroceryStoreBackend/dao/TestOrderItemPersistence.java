c0653c6ba0ebfd155584111bc849e999fa9156a5
KRouhana <37153041+KRouhana@users.noreply.github.com>
2022-02-17 15:00:44 -0500

    Create test for Order and OrderItem

---
+++ GroceryStore-Backend/src/test/java/ca/mcgill/ecse321/GroceryStoreBackend/dao/TestOrderItemPersistence.java
@@ -0,0 +1,72 @@
+package ca.mcgill.ecse321.GroceryStoreBackend.dao;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.sql.Date;
+import java.sql.Time;
+import javax.persistence.EntityManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import ca.mcgill.ecse321.GroceryStoreBackend.model.*;
+import ca.mcgill.ecse321.GroceryStoreBackend.model.Order.OrderStatus;
+import ca.mcgill.ecse321.GroceryStoreBackend.model.Order.OrderType;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class TestOrderItemPersistence {
+
+  
+  @Autowired
+  EntityManager entityManager;
+  
+  @Autowired
+  private OrderItemRepository orderItemRepository;
+  
+  @AfterEach
+  public void clearDatabase() {
+    orderItemRepository.deleteAll();
+  }
+  
+  @Test
+  public void testPersistAndLoadOrderItem() {
+    String name = "testCustomer";
+    String email = "testCustomer@mail.com";
+    String password = "testPassword";
+    String address = "town";
+    Customer customer = new Customer(email, password, name, address);
+    
+    
+    String orderId = "321";
+    OrderType orderType = OrderType.Delivery;
+    OrderStatus orderStatus = OrderStatus.Confirmed;
+    Date date = new Date(0);
+    Time time = new Time(0);
+    Order order = new Order(orderId, orderType, orderStatus, date, time, customer);
+    
+    
+    String itemName = "Milk";
+    double price = 4.65;
+    int quantityAvailable = 10;
+    ShoppableItem shoppableItem = new ShoppableItem (itemName, price, quantityAvailable);
+    
+    String orderItemId = "123";
+    int quantityWanted = 2;
+    OrderItem orderItem = new OrderItem (orderItemId, quantityWanted, shoppableItem, order);
+    
+   
+    orderItemRepository.save(orderItem);
+
+      orderItem = null;
+
+      orderItem = orderItemRepository.findOrderItemById(Integer.parseInt(orderItemId));
+      assertNotNull(orderItem);
+      assertEquals(orderItemId, orderItem.getId());
+  }
+  
+}
---
+++ GroceryStore-Backend/src/test/java/ca/mcgill/ecse321/GroceryStoreBackend/dao/TestOrderPersistence.java
@@ -0,0 +1,61 @@
+package ca.mcgill.ecse321.GroceryStoreBackend.dao;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.sql.Date;
+import java.sql.Time;
+import javax.persistence.EntityManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import ca.mcgill.ecse321.GroceryStoreBackend.model.*;
+import ca.mcgill.ecse321.GroceryStoreBackend.model.Order.OrderStatus;
+import ca.mcgill.ecse321.GroceryStoreBackend.model.Order.OrderType;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class TestOrderPersistence {
+
+  
+  @Autowired
+  EntityManager entityManager;
+  
+  @Autowired
+  private OrderRepository orderRepository;
+  
+  @AfterEach
+  public void clearDatabase() {
+      orderRepository.deleteAll();
+  }
+  
+  @Test
+  public void testPersistAndLoadOrder() {
+    String name = "testCustomer";
+    String email = "testCustomer@mail.com";
+    String password = "testPassword";
+    String address = "town";
+    Customer customer = new Customer(email, password, name, address);
+    
+    
+    String orderId = "321";
+    OrderType orderType = OrderType.Delivery;
+    OrderStatus orderStatus = OrderStatus.Confirmed;
+    Date date = new Date(0);
+    Time time = new Time(0);
+    Order order = new Order(orderId, orderType, orderStatus, date, time, customer);
+   
+      orderRepository.save(order);
+
+      order = null;
+
+      order = orderRepository.findOrderById(Integer.parseInt(orderId));
+      assertNotNull(order);
+      assertEquals(orderId, order.getId());
+  }
+  
+}
