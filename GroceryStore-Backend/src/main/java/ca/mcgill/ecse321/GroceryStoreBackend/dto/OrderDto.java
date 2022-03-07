5521044b4f23d48acf2a722ed784b556df34fb88
anaelledrai <83705526+anaelledrai@users.noreply.github.com>
2022-03-07 13:43:38 -0500

    work on dto

--- GroceryStore-Backend/src/main/java/ca/mcgill/ecse321/GroceryStoreBackend/dto/OrderDto.java
+++ GroceryStore-Backend/src/main/java/ca/mcgill/ecse321/GroceryStoreBackend/dto/OrderDto.java
@@ -2,7 +2,10 @@ package ca.mcgill.ecse321.GroceryStoreBackend.dto;
 
 import java.sql.Date;
 import java.sql.Time;
+import java.util.List;
 
+import ca.mcgill.ecse321.GroceryStoreBackend.model.Customer;
+import ca.mcgill.ecse321.GroceryStoreBackend.model.OrderItem;
 import ca.mcgill.ecse321.GroceryStoreBackend.model.Order.OrderStatus;
 import ca.mcgill.ecse321.GroceryStoreBackend.model.Order.OrderType;
 
@@ -12,15 +15,19 @@ public class OrderDto {
 	private OrderStatus orderStatus;
 	private Date date;
 	private Time time;
-
+	private CustomerDto customer;
+	private List<OrderItemDto> orderItems;
+	
 	public OrderDto() {
 		}
 
-	public OrderDto(OrderType orderType, OrderStatus orderStatus, Date date, Time time) {
+	public OrderDto(OrderType orderType, OrderStatus orderStatus, Date date, Time time, CustomerDto customer, List<OrderItemDto> orderItems) {
 			this.orderType = orderType;
 			this.orderStatus = orderStatus;
 			this.date = date;
 			this.time = time;
+			this.customer = customer;
+			this.orderItems = orderItems;
 		}
 
 	public OrderType getOrderType() {
@@ -38,5 +45,20 @@ public class OrderDto {
 	public Time getTime() {
 		return time;
 	}
+	
+	public CustomerDto getCustomer() {
+		return customer;
+	}
+	
+	public void setCustomer(CustomerDto customer) {
+		this.customer = customer;
+	}
+	
+	public List<OrderItemDto> getOrderItems() {
+		return orderItems;
+	}
 
+	public void setOrderItems(List<OrderItemDto> orderItems) {
+		this.orderItems = orderItems;
+	}
 }
