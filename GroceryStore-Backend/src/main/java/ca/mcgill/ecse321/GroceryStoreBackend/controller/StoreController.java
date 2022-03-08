7cfbf64bcb430954d003c01cfe2552b2ecdb5eb5
Matthieuhakim <77667703+Matthieuhakim@users.noreply.github.com>
2022-03-07 19:09:49 -0500

    Create Controller and Service classes

---
+++ GroceryStore-Backend/src/main/java/ca/mcgill/ecse321/GroceryStoreBackend/controller/StoreController.java
@@ -0,0 +1,5 @@
+package ca.mcgill.ecse321.GroceryStoreBackend.controller;
+
+public class StoreController {
+
+}
---
+++ GroceryStore-Backend/src/main/java/ca/mcgill/ecse321/GroceryStoreBackend/service/DailyScheduleService.java
@@ -0,0 +1,5 @@
+package ca.mcgill.ecse321.GroceryStoreBackend.service;
+
+public class DailyScheduleService {
+
+}
