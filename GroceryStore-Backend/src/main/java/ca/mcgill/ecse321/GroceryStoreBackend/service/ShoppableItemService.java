69b0905dc99aab9563f189a75a141526af071733
RalphsGit <90062463+RalphsGit@users.noreply.github.com>
2022-03-07 20:35:29 -0500

    ShoppableItem and UnavailableItem services

--- GroceryStore-Backend/src/main/java/ca/mcgill/ecse321/GroceryStoreBackend/service/ShoppableItemService.java
+++ GroceryStore-Backend/src/main/java/ca/mcgill/ecse321/GroceryStoreBackend/service/ShoppableItemService.java
@@ -1,5 +1,72 @@
 package ca.mcgill.ecse321.GroceryStoreBackend.service;
 
-public class ShoppableItemService {
+import ca.mcgill.ecse321.GroceryStoreBackend.dao.*;
+import ca.mcgill.ecse321.GroceryStoreBackend.model.*;
 
+public class ShoppableItemService {
+	
+	public ShoppableItemService() {
+		
+	}
+	
+	ShoppableItemRepository shoppableItemRepository;
+	
+	public ShoppableItem createShoppableItem(String name, double price, int quantityAvailable) {
+		
+		if(name==null || name.equals("")) throw new IllegalArgumentException("Item name cannot be blank");
+		
+		if(price<0) throw new IllegalArgumentException("Item price cannot be negative");
+		
+		nameIsValid(name);
+		
+		if(quantityAvailable<0) throw new IllegalArgumentException("Item quantity cannot be negative");
+		
+		ShoppableItem item = new ShoppableItem();
+		item.setName(name);
+		item.setPrice(price);
+		item.setQuantityAvailable(quantityAvailable);
+
+		shoppableItemRepository.save(item);
+
+		return item;
+		
+	}
+	
+	public ShoppableItem updateShoppableItem(String name, double newPrice, int newQuantityAvailable) {
+		
+		if(newPrice<0) throw new IllegalArgumentException("Item price cannot be negative");
+				
+		if(newQuantityAvailable<0) throw new IllegalArgumentException("Item quantity cannot be negative");
+		
+		ShoppableItem item = shoppableItemRepository.findByName(name);
+		
+		if(item==null)  throw new IllegalArgumentException("This item does not exist in the system");
+		
+		item.setPrice(newPrice);
+		item.setQuantityAvailable(newQuantityAvailable);
+	}
+	
+	public boolean deleteShoppableItem(String name) {
+		ShoppableItem item = shoppableItemRepository.findByName(name);
+		
+		if(item==null) throw new IllegalArgumentException("This item does not exist in the system");
+		
+		item.delete();
+		
+		return true;
+	}
+	
+	public ShoppableItem getShoppableItem(String name) {
+		return shoppableItemRepository.findByName(name);
+	}
+	
+	private void nameIsValid(String name) {
+		if(shoppableItemRepository.findByName(name)!=null) {
+			throw new IllegalArgumentException("Item name already exist");
+		}
+	}
 }
+
+
+
+
--- GroceryStore-Backend/src/main/java/ca/mcgill/ecse321/GroceryStoreBackend/service/UnavailableItemService.java
+++ GroceryStore-Backend/src/main/java/ca/mcgill/ecse321/GroceryStoreBackend/service/UnavailableItemService.java
@@ -1,5 +1,64 @@
 package ca.mcgill.ecse321.GroceryStoreBackend.service;
 
-public class UnavailableItemService {
+import ca.mcgill.ecse321.GroceryStoreBackend.dao.UnavailableItemRepository;
+import ca.mcgill.ecse321.GroceryStoreBackend.model.Item;
+import ca.mcgill.ecse321.GroceryStoreBackend.model.UnavailableItem;
 
+public class UnavailableItemService {
+	
+	UnavailableItemRepository unavailableItemRepository;
+	
+	public UnavailableItem createItem(String name, double price) {
+		
+		UnavailableItemRepository unavailableItemRepository;
+		
+		if(name==null || name.equals("")) throw new IllegalArgumentException("Item name cannot be blank");
+		
+		if(price<0) throw new IllegalArgumentException("Item price cannot be negative");
+		
+		nameIsValid(name);
+				
+		UnavailableItem item = new UnavailableItem();
+		item.setName(name);
+		item.setPrice(price);
+
+		unavailableItemRepository.save(item);
+
+		return item;
+		
+	}
+	
+	public UnavailableItem updateUnavailableItem(String name, double newPrice) {
+		
+		if(newPrice<0) throw new IllegalArgumentException("Item price cannot be negative");
+						
+		UnavailableItem item = unavailableItemRepository.findByName(name);
+		
+		if(item==null)  throw new IllegalArgumentException("This item does not exist in the system");
+		
+		item.setPrice(newPrice);
+		
+		
+	}
+	
+	public boolean deleteUnavailableItem(String name) {
+		UnavailableItem item = unavailableItemRepository.findByName(name);
+		
+		if(item==null) throw new IllegalArgumentException("This item does not exist in the system");
+		
+		item.delete();
+		
+		return true;
+	}
+	
+	public UnavailableItem getUnavailableItem(String name) {
+		return unavailableItemRepository.findByName(name);
+	}
+	
+	
+	private void nameIsValid(String name) {
+		if(unavailableItemRepository.findByName(name)!=null) {
+			throw new IllegalArgumentException("Item name already exist");
+		}
+	}
 }
